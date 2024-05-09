3436a3b17ee2b433a03a43cd437bfc67e1e63192
Maimoona Masood Ferozi <132249128+Maimoonagit123@users.noreply.github.com>
2024-05-09 21:23:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var week = "Friday";
+switch (week) {
+    case "Monday":
+        console.log("Its working day Go to work.");
+        break;
+    case "Tuesday":
+        console.log("Go to Office sharp 8pm.");
+        break;
+    case "Wednesday":
+        console.log("Go to shopping");
+        break;
+    case "Thursday":
+        console.log("Go to cousins wedding");
+        break;
+    case "Friday":
+        console.log("Pray Namaz");
+        break;
+    case "Saturday":
+        console.log("Go to Picnic wih collegues");
+        break;
+    case "Sunday":
+        console.log("Go to Hunting");
+        break;
+}
